b8abc079965b8377fa9055606459244736f5669f
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-11-14 07:38:59 +0530

    intro page in progress

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/header";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <div>
+      <Intro/>
+     </div>
     </div>
   );
 }
 
 export default App;
+
